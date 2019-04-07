6ee18eb9ec8613b96d01b88a40aff1eacb350d31
Norman ADDA <nadda@upper-link.com>
2019-04-07 19:43:48 +0200

    Add sample for App Service in Docker

--- Scripts_ManageDotNetCore.bash
+++ Scripts_ManageDotNetCore.bash
@@ -13,3 +13,8 @@ git commit -m "Initial commit"
 git remote add origin https://ftpNadda@nadda-bestbike.scm.azurewebsites.net:443/nadda-bestbike.git #Specify the remote repo
 git remote -v #Check the remote repo
 git push origin master #Push to master branch
+
+
+#build a container for an app service
+git clone https://github.com/MicrosoftDocs/mslearn-deploy-run-container-app-service.git #Get a sample of app services solution with a docker file
+az acr build --registry "naddatst01" --image webimage . #Send the contant of current folder in the specified registry and execute docker file
